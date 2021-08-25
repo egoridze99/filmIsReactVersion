c39c0f9de1f88d161a20647ffd061843e71c6bfa
Egor Telnov <egor.telnov@mvideo.ru>
2021-08-25 10:13:22 +0300

    Изменения

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,76 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import './index.scss';
+import "./index.scss";
 
-const Prices = props => {
-    return (  
-        <section className="price-section">
-          <a name="price"></a>
-          <h2 className="section__title" data-scrolly-down="fadeInDownBig">
-            Цена аренды зала 
-          </h2>
-          <div className="price-section__container" data-scrolly-down="fadeIn">
-            <div className="price-section__top price-section__wrapper">
-              <h3 className="price-section__title">
-                С 12:00 до 18:00
-              </h3>
-              <div className="wrapper__container">
-                <div className="container__solo">
-                  <h3 className="container__title">
-                    Для двоих
-                  </h3>
-                  <p className="container__price">
-                    600&#8381;
-                  </p>
-                  <p className="container__info">
-                    (За двоих)
-                  </p>
-                </div>
-                <div className="container__company">
-                  <h3 className="container__title">
-                    Для компаний
-                  </h3>
-                  <p className="container__price">
-                    250&#8381;
-                  </p>
-                  <p className="container__info">
-                    (С человека)
-                  </p>
-                </div>
-              </div>
+const Prices = (props) => {
+  return (
+    <section className="price-section">
+      <a name="price"></a>
+      <h2 className="section__title" data-scrolly-down="fadeInDownBig">
+        Цена аренды зала
+      </h2>
+      <div className="price-section__container" data-scrolly-down="fadeIn">
+        <div className="price-section__top price-section__wrapper">
+          <h3 className="price-section__title">С 12:00 до 18:00</h3>
+          <div className="wrapper__container">
+            <div className="container__solo">
+              <h3 className="container__title">Для двоих</h3>
+              <p className="container__price">600&#8381;</p>
+              <p className="container__info">(За двоих)</p>
             </div>
-            <div className="price-section__bottom price-section__wrapper">
-              <h3 className="price-section__title">
-                С 18:00 до 04:00
-              </h3>
-              <div className="wrapper__container">
-                <div className="container__solo">
-                  <h3 className="container__title">
-                    Для двоих
-                  </h3>
-                  <p className="container__price">
-                    800&#8381;
-                  </p>
-                  <p className="container__info">
-                    (За двоих)
-                  </p>
-                </div>
-                <div className="container__company">
-                  <h3 className="container__title">
-                    Для компаний
-                  </h3>
-                  <p className="container__price">
-                    350&#8381;
-                  </p>
-                  <p className="container__info">
-                    (С человека)
-                  </p>
-                </div>
-              </div>    
+            <div className="container__company">
+              <h3 className="container__title">Для компаний</h3>
+              <p className="container__price">250&#8381;</p>
+              <p className="container__info">(С человека)</p>
             </div>
           </div>
-        </section>
-    );
-}
- 
+        </div>
+        <div className="price-section__bottom price-section__wrapper">
+          <h3 className="price-section__title">С 18:00 до 04:00</h3>
+          <div className="wrapper__container">
+            <div className="container__solo">
+              <h3 className="container__title">Для двоих</h3>
+              <p className="container__price">800&#8381;</p>
+              <p className="container__info">(За двоих)</p>
+            </div>
+            <div className="container__company">
+              <h3 className="container__title">Для компаний</h3>
+              <p className="container__price">350&#8381;</p>
+              <p className="container__info">(С человека)</p>
+            </div>
+          </div>
+        </div>
+        <p className="container__price-info">
+          *Цена указана за 2 часа аренды зала
+        </p>
+      </div>
+    </section>
+  );
+};
+
 export default Prices;
