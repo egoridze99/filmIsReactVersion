22fcb6ebcd744e57e39e43701bed20ac18573a9d
Egori <egoridze81@gmail.com>
2024-06-22 13:09:41 +0300

    Редирект на вк

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-import React, { useEffect } from "react";
-
-import Preloader from "./components/Preloader/Preloader";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import HeroContainer from "./components/Hero/HeroContainer";
-import ModalContainer from "./components/Modal/ModalContainer";
-import Rooms from "./components/Rooms/Rooms";
-import Prices from "./components/Prices/Prices";
-import Datings from "./components/Datings/Datings";
-import Features from "./components/Features/Features";
-import Caraoke from "./components/Caraoke/Caraoke";
-import SalesContainer from "./components/Sales/SalesContainer";
-import Map from "./components/Map/Map";
-import Media from "./components/Media/Media";
-import Footer from "./components/Footer/Footer";
+import React from "react";
 
 import "./index.scss";
 import "animate.css/animate.css";
-import useScrollAnimation from "./helpers/hooks/useScrollAnimation";
-import useDisableAtachment from "./helpers/hooks/useDisableAtachment";
 
-function App(props) {
-    const { isLoaded } = props;
+function App() {
+    React.useState(() => {
+        window.location.replace("https://vk.com/film_is_chita")
+    })
 
-    useScrollAnimation();
-    useDisableAtachment();
 
-    const content = (
-        <>
-            <HeaderContainer />
-            <ModalContainer />
-            <div className='wrapper'>
-                <a id='main'></a>
-                <HeroContainer />
-                {/* <Rooms /> */}
-                <Prices />
-                <Datings />
-                <Features />
-                <Caraoke />
-                <SalesContainer />
-                <Map />
-                <Media />
-                <Footer />
-            </div>
-        </>
-    );
-
-    return <>{!isLoaded ? <Preloader></Preloader> : content}</>;
+    return <div />;
 }
 
 export default App;
